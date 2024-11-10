ecc2105017ff422b3972fc08a022de825121ae21
haydn202 <haydnthomson@hotmail.com>
2024-11-10 12:23:13 -0600

    comments

--- API/Entities/Post.cs
+++ API/Entities/Post.cs
@@ -5,9 +5,9 @@ public class Post
     public Guid Id { get; init; }
     public required string Title { get; set; }
     public required string Description { get; set; }
-    public string ThumbnailUrl { get; set; }
+    public string ThumbnailUrl { get; set; } // probably should set this up to go to blob storage too.
     public List<Topic> Topics { get; set; }
     public DateTime CreatedOn { get; set; } = DateTime.Now;
-    public required string Content { get; set; }
+    public required string Content { get; set; } // will change to be in blob storage.
     public List<Comment>? MainComments { get; set; }
 }
