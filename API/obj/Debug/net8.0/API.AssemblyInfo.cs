8ef94f648161f5e1c4cb2a20a2a75131b8901293
haydn202 <haydnthomson@hotmail.com>
2024-11-11 14:59:51 -0600

    dont auto open browser

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc2105017ff422b3972fc08a022de825121ae21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36433e2c6cec798f80db13e313e138ce5635a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
