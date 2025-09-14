cc6ad69f033eeb1ec60ed0d40cf54a41de91cc19
haydn202 <haydnthomson@hotmail.com>
2025-09-14 22:21:52 +1200

    messing with text

---
+++ client/src/features/text-editor/text-editor.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {QuillEditorComponent} from 'ngx-quill';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-text-editor',
+  templateUrl: './text-editor.html',
+  imports: [
+    QuillEditorComponent,
+    FormsModule
+  ],
+  styleUrls: ['./text-editor.css']
+})
+export class TextEditor {
+  editorContent = '';
+
+  modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      ['blockquote', 'code-block'],
+      ['link', 'image', 'video'],
+      [{ 'align': [] }],
+      [{ 'color': [] }, { 'background': [] }],
+      ['clean']
+    ]
+  };
+
+  publish() {
+    console.log('Blog Content:', this.editorContent);
+    alert('Published! Check the console for output.');
+  }
+}
